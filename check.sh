07b61ed8bb4560108dea7fb6e98a9aaf8bceaab0
Fedor Logachev <not.fl3@gmail.com>
2021-05-27 16:03:14 -0500

    keep removing things

--- check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-cargo test --workspace --all-targets --all-features
-cargo check --workspace --all-targets --all-features
-cargo check --lib --target wasm32-unknown-unknown --all-features
-CARGO_INCREMENTAL=0 cargo clippy --workspace --all-targets --all-features --  -D warnings -W clippy::all
-cargo fmt --all -- --check
